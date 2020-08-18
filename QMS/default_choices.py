310ecadac7a447d052f39505c241c3cc4db363dc
Nithi07 <53622572+Nithi07@users.noreply.github.com>
2020-08-18 11:02:39 +0530

    Add files via upload

--- QMS/default_choices.py
+++ QMS/default_choices.py
@@ -1,3 +1,16 @@
+"""Employee Details form Choices"""
+
+employ_status = (
+        ('','---Select---'),
+        (0,'Active'),
+        (1,'Inactive'),
+        (2,'Hold'),
+        (3,'Settlement')
+    )
+
+#""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+
 """Audit_comments form Choices"""
 
 status = (
@@ -37,3 +50,40 @@ type = (
     ('Project','Project'),
     ('Tender','Tender')
     )
+
+
+
+    # """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+
+"""Project Details Form"""
+
+type = (
+    (1,'Project'),
+    (2,'Tender')
+)
+project_status=(
+    (0,'Active'),
+    (1,'Inactive'),
+    (2,'Hold'),
+    (3,'Cancelled')
+)
+
+
+
+# """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+
+
+"""AuditSchdule Model"""
+
+audit_comment_status = (
+        (0,'Yet to start'),
+        (1,'Auditor and Auditee to Respond'),
+        (2,'Auditor to Respond'),
+        (3,'Auditee to Respond'),
+        (4,'Auditor to Verify'),
+        (5,'MR to Respond'),
+        (6,'Waiting for Closed'),
+        (7,'Completed')
+)
--- QMS/urls.py
+++ QMS/urls.py
@@ -5,6 +5,7 @@ from QMS.views import auditschedule
 from QMS.views import empdetails
 from QMS.views import workmanual
 from QMS.views import auditcomment
+from QMS.views import project_teamwise
 
 
 
@@ -16,42 +17,53 @@ urlpatterns = [
 
     path('homepage/',common.HomePage.as_view(),name='homepage' ),
 
+
     path('audittypeview/',common.AudittypeListview.as_view(),name='audittypeview' ),
     path('audittypecreate/',common.AudittypeCreate.as_view(),name='audittypecreate' ),
     path('audittypeupdate/<int:pk>/',common.AudittypeUpdate.as_view(),name='audittypeupdate'),
-    path('audittypedelete/<int:pk>/',common.AudittypeDelete.as_view(),name='audittypedelete' ),
+    path('audittypedelete/<int:pk>/',common.AudittypeDelete.ObjectDelete,name='audittypedelete' ),
+
 
     path('emppositionview/',common.EmployePositionListview.as_view(),name='emppositionview' ),
     path('emppositionform/',common.EmployePositionCreate.as_view(),name='emppositionform' ),
     path('emppositionupdate/<int:pk>/',common.EmployePositionUpdate.as_view(),name='emppositionupdate'),
-    path('emppositiondelete/<int:pk>/',common.EmployePositionDelete.as_view(),name='emppositiondelete' ),
+    path('emppositiondelete/<int:pk>/',common.EmployePositionDelete.ObjectDelete,name='emppositiondelete' ),
+
 
     path('employedeptview/',common.EmployeDepartmentListview.as_view(),name='employedeptview' ),
     path('employedeptform/',common.EmployeDepartmentCreate.as_view(),name='employedeptform' ),
     path('employedeptupdate/<int:pk>/',common.EmployeDepartmentUpdate.as_view(),name='employedeptupdate'),
-    path('employedeptdelete/<int:pk>/',common.EmployeDepartmentDelete.as_view(),name='employedeptdelete' ),
+    path('employedeptdelete/<int:pk>/',common.EmployeDepartmentDelete.ObjectDelete,name='employedeptdelete' ),
+
 
     path('auditorlistform/',auditorlist.AuditorlistCreate.as_view(),name='auditorlistform'),
     path('auditorlistview/',auditorlist.AuditorlistListview.as_view(),name='auditorlistview'),
-    path('auditorlistupdate/<int:pk>/',auditorlist.AuditorlistUpdate.as_view(),name='auditorlistupdate'),
-    path('auditorlistdelete/<int:pk>/',auditorlist.AuditorlistDelete.as_view(),name='auditorlistdelete'),
+    path('auditorlistdelete/<int:pk>/',auditorlist.AuditorlistDelete.ObjectDelete,name='auditorlistdelete'),
 
-    path('auditorautocomplete/',auditschedule.ajax_response,name='auditorautocomplete'),
-    path('projectyperautocomplete/',auditschedule.ajax_hidden,name='projectyperautocomplete'),
-    path('projectcreate/',auditschedule.ProjectCreate.as_view(),name='projectcreate'),
-    path('projectupdate/<int:pk>/',auditschedule.ProjectUpdate.as_view(),name='projectupdate'),
-    path('projectview/',auditschedule.ProjectListview.as_view(),name='projectview'),
-    path('teamwisecreate/',auditschedule.TeamwiseCreate.as_view(),name='teamwisecreate'),
-    path('teamwiseupdate/<int:pk>/',auditschedule.TeamwiseUpdate.as_view(),name='teamwiseupdate'),
-    path('teamwiseview/',auditschedule.TeamwiseListview.as_view(),name='teamwiseview'),
-    # path('ajaxproject/',auditschedule.ajax_load_project,name='ajaxproject'),
+
+    path('projectcreate/',project_teamwise.ProjectCreate.as_view(),name='projectcreate'),
+    path('projectupdate/<int:pk>/',project_teamwise.ProjectUpdate.as_view(),name='projectupdate'),
+    path('projectview/',project_teamwise.ProjectListview.as_view(),name='projectview'),
+    path('projectdelete/<int:pk>/',project_teamwise.ProjectDelete.ObjectDelete,name='projectdelete'),
+    path('teamwisecreate/',project_teamwise.TeamwiseCreate.as_view(),name='teamwisecreate'),
+    path('teamwiseupdate/<int:pk>/',project_teamwise.TeamwiseUpdate.as_view(),name='teamwiseupdate'),
+    path('teamwiseview/',project_teamwise.TeamwiseListview.as_view(),name='teamwiseview'),
+    path('teamwisedelete/<int:pk>/',project_teamwise.TeamwiseDelete.ObjectDelete,name='teamwisedelete'),
+
+
+    path('auditeeautocomplete/',auditschedule.ajax_response,name='auditeeautocomplete'),
+    path('projectautocomplete/',auditschedule.ajax_hidden,name='projectautocomplete'),
     path('auditscheduleform/',auditschedule.AuditscheduleCreate.as_view(),name='auditscheduleform'),
-    path('mrconfirmschedule/',auditschedule.Sendmail,name='mrconfirmschedule'),
+    path('mrconfirmschedule/',auditschedule.MRconfirm.as_view(),name='mrconfirmschedule'),
+    path('mrconfirmmail/',auditschedule.MRconfirm.Sendmail,name='mrconfirmmail'),
     path('auditscheduleview/',auditschedule.AuditscheduleListview.as_view(),name='auditscheduleview'),
     path('auditscheduleconfirmview/<int:pk>/',auditschedule.AuditscheduleConfirmListview.as_view(),name='auditscheduleconfirmview'),
+                                            # passing string from template using url
+    path(r'^confirmingmanagers/(?:(?P<manager_name>.+)/)?$',auditschedule.AuditscheduleConfirmListview.save,name="confirmingmanagers"),
     path('auditscheduleupdate/<int:pk>/',auditschedule.AuditscheduleUpdate.as_view(),name='auditscheduleupdate'),
-    path('auditscheduledelete/<int:pk>/',auditschedule.AuditscheduleDelete.as_view(),name='auditscheduledelete'),
-    path('auditschedulepostpond/<int:pk>/',auditschedule.AuditschedulePostpond.as_view(),name='auditschedulepostpond'),
+    path('auditscheduledelete/<int:pk>/',auditschedule.AuditscheduleDelete.ObjectDelete,name='auditscheduledelete'),
+    path('auditschedulepostpondcreate/<int:pk>/',auditschedule.AuditschedulePostpond.as_view(),name='auditschedulepostpondcreate'),
+    path('auditschedulepostpondupdate/<int:pk>/',auditschedule.AuditschedulePostpondUpdate.as_view(),name='auditschedulepostpondupdate'),
     path('auditschedulecancel/<int:pk>/',auditschedule.AuditscheduleCancel.as_view(),name='auditschedulecancel'),
     path('auditscheduleclosed/<int:id>/',auditschedule.AuditscheduleClosed,name='auditscheduleclosed'),
     path('auditschedulereporttable/',auditschedule.AuditscheduleExportTable.as_view(),name='auditschedulereporttable'),
@@ -68,29 +80,23 @@ urlpatterns = [
     path('sendmailauditor/<int:id>/',auditcomment.AuditorcommentCreate.Sendmail,name='sendmailauditor'),
     path('auditorcommentview/<int:pk>/',auditcomment.MrcommentListCreate.as_view(),name='auditorcommentview'),
     path('sendmailmr/<int:id>/',auditcomment.MrcommentListCreate.Sendmail,name='sendmailmr'),
-    # path('mrcommentupdate/<int:pk>/',auditcomment.MrcommentListUpdate.as_view(),name='mrcommentupdate'),
-    # path('auditorcommentaddnew/',auditcomment.Auditorcommentaddnew.as_view(),name='auditorcommentaddnew'),
-    path('auditorcommentdelete/<int:pk>/',auditcomment.AuditorcommentDelete.as_view(),name='auditorcommentdelete'),
-
     path('auditeecommentform/<int:pk>/',auditcomment.AuditeecommentCreate.as_view(),name='auditeecommentform'),
     path('sendmailauditee/<int:id>/',auditcomment.AuditeecommentCreate.Sendmail,name='sendmailauditee'),
-    # path('auditeecommentupdate/<int:pk>/',auditcomment.AuditeecommentUpdate.as_view(),name='auditeecommentupdate'),
     path('verifycommentform/<int:pk>/',auditcomment.VerifycommentCreate.as_view(),name='verifycommentform'),
-    path('verifyajax/',auditcomment.load_cities,name='verifyajax'),
+    path('verifyajax/',auditcomment.load_page,name='verifyajax'),
     path('sendmailverify/<int:id>/',auditcomment.VerifycommentCreate.Sendmail,name='sendmailverify'),
-    # path('verifycommentupdate/<int:pk>/',auditcomment.VerifycommentUpdate.as_view(),name='verifycommentupdate'),
 
 
     path('empdetailsform/',empdetails.EmployeeDetailsCreate.as_view(),name='empdetailsform'),
     path('empdetailsview/',empdetails.EmployeeDetailsListview.as_view(),name='empdetailsview'),
     path('empdetailsupdate/<int:pk>/',empdetails.EmployeeDetailsUpdate.as_view(),name='empdetailsupdate'),
-    path('empdetailsdelete/<int:pk>/',empdetails.EmployeeDetailsDelete.as_view(),name='empdetailsdelete'),
+    path('empdetailsdelete/<int:pk>/',empdetails.EmployeeDetailsDelete.ObjectDelete,name='empdetailsdelete'),
+
 
     path('workmanualform/',workmanual.WorkManualCreate.as_view(),name='workmanualform'),
     path('workmanualview/',workmanual.WorkManualListview.as_view(),name='workmanualview'),
     path('workmanualupdate/<int:pk>/',workmanual.WorkManualUpdate.as_view(),name='workmanualupdate'),
-    path('workmanualdelete/<int:pk>/',workmanual.WorkManuaDelete.as_view(),name='workmanualdelete'),
+    path('workmanualdelete/<int:pk>/',workmanual.WorkManuaDelete.ObjectDelete,name='workmanualdelete'),
     path('workmanualaddnew/<int:pk>/',workmanual.WorkManualaddnewCreate.as_view(),name='workmanualaddnew'),
-
 
    ]
